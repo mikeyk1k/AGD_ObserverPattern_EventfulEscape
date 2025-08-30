dad7c3c7b1a6cdef1cc7d691c49197294fc37bfb
Mikey <mikeylearn430@gmail.com>
2025-08-30 18:21:45 +0700

    Adding Event Architecture

---
+++ Assets/Scripts/Events/EventController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class EventController
+{
+    private Action baseEvent;
+    public void AddListener(Action listener) => baseEvent += listener;
+    public void RemoveListener(Action listener) => baseEvent -= listener;
+    public void InvokeEvent() => baseEvent?.Invoke();
+}
---
+++ Assets/Scripts/Events/EventService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EventService
+{
+    private static EventService instance;
+    public static EventService Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new EventService();
+            }
+            return instance;
+        }
+    }
+    public EventController OnLightSwitchToggled { get; private set; }
+    public EventService()
+    {
+        OnLightSwitchToggled = new EventController();
+    }
+}
