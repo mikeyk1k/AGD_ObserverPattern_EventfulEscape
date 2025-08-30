404b45fd7be32012b2a98a69a4b30322240c7b69
Mikey <mikeylearn430@gmail.com>
2025-08-30 17:43:25 +0700

    Converting Delegate into Event Action

--- Assets/Scripts/Interactables/LightSwitchView.cs
+++ Assets/Scripts/Interactables/LightSwitchView.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using static LightSwitchView;
@@ -6,16 +7,16 @@ public class LightSwitchView : MonoBehaviour, IInteractable
 {
     [SerializeField] private List<Light> lightsources = new List<Light>();
     private SwitchState currentState;
-    public delegate void LightSwitchDelegate();
-    public static LightSwitchDelegate lightToggled;
 
-    private void OnEnable() => lightToggled += onLightSwitch;
+    public static event Action LightToggled;
 
-    private void OnDisable() => lightToggled -= onLightSwitch;
+    private void OnEnable() => LightToggled += onLightSwitch;
+
+    private void OnDisable() => LightToggled -= onLightSwitch;
 
     private void Start() => currentState = SwitchState.Off;
 
-    public void Interact() => lightToggled?.Invoke();
+    public void Interact() => LightToggled?.Invoke();
 
     private void toggleLights()
     {
